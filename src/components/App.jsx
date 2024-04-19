6080c40aab300d22ea1d51cdb68b70aff8d0740f
Hatori <ahgangadm@gmail.com>
2024-04-19 19:13:37 +0300

    Add basic markup and functionality

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,35 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import { getData } from "../gallery-image";
+import ErrorMessage from "./ErrorMessage/ErrorMessage";
+import Loader from "./Loader/Loader";
+import SearchBar from "./SearchBar/SearchBar";
+import ImageGallery from "./ImageGallery/ImageGallery";
+import LoadMoreBtn from "./LoadMoreBtn/LoadMoreBtn";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [pictures, setPictures] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const [page, setPage] = useState(1);
+  const [keyWord, setKeyWord] = useState("");
+
+  const handleSubmit = async (value) => {
+    try {
+      setPictures([]);
+      setIsLoading(true);
+      const data = await getData(value, page);
+      setPictures(data);
+    } catch (error) {
+      setError(true);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  console.log(pictures);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <SearchBar onSubmit={handleSubmit} />
+      {pictures.length > 0 && <ImageGallery items={pictures} />}
+      {isLoading && <Loader />}
+      {error && <ErrorMessage />}
+      {pictures.length > 0 && page >= 1 && <LoadMoreBtn />}
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;

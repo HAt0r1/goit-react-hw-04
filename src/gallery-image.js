63b443f415610f7259476f4e429db14417521c92
Hatori <ahgangadm@gmail.com>
2024-04-20 19:02:22 +0300

    Rewrite state variable

--- src/gallery-image.js
+++ src/gallery-image.js
@@ -3,11 +3,11 @@ import axios from "axios";
 axios.defaults.baseURL = "https://api.unsplash.com";
 const API_KEY = "OzfFjjr5vwIJ6deZFl4Ce03MfUs6JxE5pSRVoXQLK_w";
 
-export const getData = async (value, page) => {
+export const getData = async (keyWord, page) => {
   const responce = await axios.get("/search/photos", {
     params: {
       client_id: API_KEY,
-      query: value,
+      query: keyWord,
       page: page,
       per_page: 12,
     },

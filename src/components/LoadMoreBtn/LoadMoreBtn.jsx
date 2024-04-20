fc2489419a0ec008741f92d56501c20e46866449
Hatori <ahgangadm@gmail.com>
2024-04-20 19:03:47 +0300

    Add event function in button

--- src/components/LoadMoreBtn/LoadMoreBtn.jsx
+++ src/components/LoadMoreBtn/LoadMoreBtn.jsx
@@ -1,9 +1,9 @@
 import style from "./LoadMoreBtn.module.css";
 
-const LoadMoreBtn = () => {
+const LoadMoreBtn = ({ onClick }) => {
   return (
     <div className={style.container}>
-      <button className={style.button} type="button">
+      <button onClick={onClick} className={style.button} type="button">
         Load More
       </button>
     </div>

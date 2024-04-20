62812f819d00413a33eaffa50d293dc480d75878
Hatori <ahgangadm@gmail.com>
2024-04-20 19:04:23 +0300

    Rewrite component

--- src/components/ImageCard/ImageCard.jsx
+++ src/components/ImageCard/ImageCard.jsx
@@ -1,22 +1,10 @@
 import style from "./ImageCard.module.css";
 
-const ImageCard = ({ items }) => {
+const ImageCard = ({ pic, alt }) => {
   return (
-    <>
-      {items.map(({ id, alt_description, urls: { small } }) => {
-        return (
-          <li key={id} className={style.pictureItem}>
-            <div className={style.pictureContainer}>
-              <img
-                className={style.picture}
-                src={small}
-                alt={alt_description}
-              />
-            </div>
-          </li>
-        );
-      })}
-    </>
+    <div className={style.pictureContainer}>
+      <img className={style.picture} src={pic} alt={alt} />
+    </div>
   );
 };
 

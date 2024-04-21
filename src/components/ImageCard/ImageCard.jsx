74919640d50f74730d243a69b0e75be03aaf0c74
Hatori <ahgangadm@gmail.com>
2024-04-21 18:31:34 +0300

    Add function that open modal window

--- src/components/ImageCard/ImageCard.jsx
+++ src/components/ImageCard/ImageCard.jsx
@@ -1,8 +1,8 @@
 import style from "./ImageCard.module.css";
 
-const ImageCard = ({ pic, alt }) => {
+const ImageCard = ({ pic, alt, onOpen }) => {
   return (
-    <div className={style.pictureContainer}>
+    <div onClick={onOpen} className={style.pictureContainer}>
       <img className={style.picture} src={pic} alt={alt} />
     </div>
   );

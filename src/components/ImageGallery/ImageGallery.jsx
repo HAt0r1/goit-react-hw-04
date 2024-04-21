4ff82605283911d10949ed17abab3d8d04ab87d8
Hatori <ahgangadm@gmail.com>
2024-04-21 18:33:05 +0300

    Destructuring of variables

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -2,13 +2,19 @@ import ImageCard from "../ImageCard/ImageCard";
 
 import style from "./ImageGallery.module.css";
 
-const ImageGallery = ({ items }) => {
+const ImageGallery = ({ items, onOpen, modalData }) => {
   return (
     <ul className={style.list}>
-      {items.map(({ id, alt_description, urls: { small } }) => {
+      {items.map(({ id, alt_description, urls: { small, regular } }) => {
         return (
-          <li className={style.pictureItem} key={id}>
-            <ImageCard pic={small} alt={alt_description} />
+          <li
+            onClick={() => {
+              modalData({ url: regular, alt: alt_description });
+            }}
+            className={style.pictureItem}
+            key={id}
+          >
+            <ImageCard onOpen={onOpen} pic={small} alt={alt_description} />
           </li>
         );
       })}
